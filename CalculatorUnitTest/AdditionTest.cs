4c3fe17e0e6b9b809807e961f53ff78f42341e49
soorajmohan93 <soorajmohan93@gmail.com>
2022-02-21 14:21:49 -0500

    Comments in Addition Test
    Explanatory comments for each test in AdditionTest class

--- CalculatorUnitTest/AdditionTest.cs
+++ CalculatorUnitTest/AdditionTest.cs
@@ -14,38 +14,42 @@ namespace CalculatorUnitTest
             _calculator = new Calculator();
         }
 
-        //Test Methods
+        //Test Method to add two positive integers (Test1)
         [TestMethod]
         public void Test_Add_Two_Positive_Numbers()
         {
             Assert.AreEqual(12.0, _calculator.Add(10.0, 2.0));
         }
 
+        //Test Method to add two negative integers (Test2)
         [TestMethod]
         public void Test_Add_Two_Negative_Numbers()
         {
             Assert.AreEqual(-15.0, _calculator.Add(-10.0, -5.0));
         }
 
+        //Test Method to add one positive and one negative integers (Test3)
         [TestMethod]
         public void Test_Add_One_Positive_One_Negative_Numbers()
         {
             Assert.AreEqual(8.0, _calculator.Add(12.0, -4.0));
         }
 
+        //Test Method to add one negative and one positive integers (Test4)
         [TestMethod]
         public void Test_Add_One_Negative_One_Positive_Numbers()
         {
             Assert.AreEqual(-4.0, _calculator.Add(-12.0, 8.0));
         }
 
+        //Test Method to add two positive decimal numbers (Test5)
         [TestMethod]
         public void Test_Add_Two_Decimal_Numbers()
         {
             Assert.AreEqual(17.1, _calculator.Add(12.5, 4.6));
         }
 
-        //Data Test Methods
+        //Data Test Method to add two positive integer values (Test6)
         [TestMethod]
         [DataRow(10.0, 2.0)]
         [DataRow(2.0, 3.0)]
@@ -54,6 +58,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first + second, _calculator.Add(first, second));
         }
 
+        //Data Test Method to add two positive decimal values (Test7)
         [TestMethod]
         [DataRow(10.43, 2.2)]
         [DataRow(2.4, 3.5)]
@@ -62,6 +67,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first + second, _calculator.Add(first, second));
         }
 
+        //Data Test Method to add two negative decimal values (Test8)
         [TestMethod]
         [DataRow(-10.6, -8.2)]
         [DataRow(-3.2, -7.524)]
@@ -70,6 +76,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first + second, _calculator.Add(first, second));
         }
 
+        //Data Test Method to add one negative decimal and one positive integer values (Test9)
         [TestMethod]
         [DataRow(-14.624, 8)]
         [DataRow(-1.2, 6)]
@@ -78,6 +85,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first + second, _calculator.Add(first, second));
         }
 
+        //Data Test Method to add one positive integer and one positive decimal values (Test10)
         [TestMethod]
         [DataRow(12, 8.453)]
         [DataRow(5, 6.934)]
