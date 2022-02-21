14dc99970c7c3f2c660b1f617757b26bdda13439
soorajmohan93 <soorajmohan93@gmail.com>
2022-02-21 14:33:20 -0500

    Comments in Multiplication Test
    Explanatory Comments updated in MultiplicationTest class

--- CalculatorUnitTest/MultiplicationTest.cs
+++ CalculatorUnitTest/MultiplicationTest.cs
@@ -15,38 +15,42 @@ namespace CalculatorUnitTest
             _calculator = new Calculator();
         }
 
-        //Test Methods
+        //Test Method to multiply two positive integers (Test1)
         [TestMethod]
         public void Test_Multiply_Two_Positive_Numbers()
         {
             Assert.AreEqual(20.0, _calculator.Multiply(10.0, 2.0));
         }
 
+        //Test Method to multiply two negative integers (Test2)
         [TestMethod]
         public void Test_Multiply_Two_Negative_Numbers()
         {
             Assert.AreEqual(50.0, _calculator.Multiply(-10.0, -5.0));
         }
 
+        //Test Method to multiply one positive and one negative integers (Test3)
         [TestMethod]
         public void Test_Multiply_One_Positive_One_Negative_Numbers()
         {
             Assert.AreEqual(-8.0, _calculator.Multiply(2.0, -4.0));
         }
 
+        //Test Method to multiply one negative and one positive integers (Test4)
         [TestMethod]
         public void Test_Multiply_One_Negative_One_Positive_Numbers()
         {
             Assert.AreEqual(-16.0, _calculator.Multiply(-2.0, 8.0));
         }
 
+        //Test Method to multiply two positive decimal numbers (Test5)
         [TestMethod]
         public void Test_Multiply_Two_Decimal_Numbers()
         {
             Assert.AreEqual(8.25, _calculator.Multiply(2.5, 3.3));
         }
 
-        //Data Test Methods
+        //Data Test Method to multiply two positive integer values (Test6)
         [TestMethod]
         [DataRow(10.0, 2.0)]
         [DataRow(2.0, 3.0)]
@@ -55,6 +59,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first * second, _calculator.Multiply(first, second));
         }
 
+        //Data Test Method to multiply two positive decimal values (Test7)
         [TestMethod]
         [DataRow(10.43, 2.2)]
         [DataRow(2.4, 3.5)]
@@ -63,6 +68,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first * second, _calculator.Multiply(first, second));
         }
 
+        //Data Test Method to multiply two negative decimal values (Test8)
         [TestMethod]
         [DataRow(-10.6, -8.2)]
         [DataRow(-3.2, -7.524)]
@@ -71,6 +77,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first * second, _calculator.Multiply(first, second));
         }
 
+        //Data Test Method to multiply one negative decimal and one positive integer values (Test9)
         [TestMethod]
         [DataRow(-14.624, 8)]
         [DataRow(-1.2, 6)]
@@ -79,6 +86,7 @@ namespace CalculatorUnitTest
             Assert.AreEqual(first * second, _calculator.Multiply(first, second));
         }
 
+        //Data Test Method to multiply one positive integer and one positive decimal values (Test10)
         [TestMethod]
         [DataRow(12, 8.453)]
         [DataRow(5, 6.934)]
