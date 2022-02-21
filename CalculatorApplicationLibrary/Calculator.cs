76e84f4ccbd343892e1633520060638968607e4b
soorajmohan93 <soorajmohan93@gmail.com>
2022-02-21 14:14:10 -0500

    Comments for Calculator Library Code
    Updated Explanatory comments for ASP.net Core Library Code

--- CalculatorApplicationLibrary/Calculator.cs
+++ CalculatorApplicationLibrary/Calculator.cs
@@ -4,6 +4,7 @@
   Subtraction, Multiplication and Division*/
 namespace CalculatorApplicationLibrary
 {
+    //Class containing method for all four operations
     public class Calculator
     {
         //Method to add two numbers
@@ -28,6 +29,7 @@ namespace CalculatorApplicationLibrary
         public double? Divide(double first, double second)
         {
             decimal result = 0.0M;
+
             //try catch block to handle division by zero exceptions
             try
             {
@@ -36,6 +38,7 @@ namespace CalculatorApplicationLibrary
             catch (DivideByZeroException ex)
             {
                 Console.WriteLine("Divide by 0 is not valid! Error: " + ex);
+                //the below exception will be caught by the test method and WebAPI calls
                 throw new DivideByZeroException();
             }
             return (double)result;
@@ -48,30 +51,37 @@ namespace CalculatorApplicationLibrary
             double b = 0.0;
             string userEntry;
 
-            //User to enter two values
+            //User to enter first value in console
             Console.WriteLine("Enter First Value");
             userEntry = Console.ReadLine();
             a = Convert.ToDouble(userEntry);
 
+            //User to enter first value in console
             Console.WriteLine("Enter Second Value");
             userEntry = Console.ReadLine();
             b = Convert.ToDouble(userEntry);
 
             var calculate = new Calculator();
 
-            //Calling all four methods
+            //Calling all four methods and displaying in console
             Console.WriteLine("Results");
             Console.WriteLine("-------");
+
+            //Addition
             Console.WriteLine("Addition: " + a + " + " + b + " = " + calculate.Add(a, b));
+
+            //Subtraction
             Console.WriteLine("Subtraction: " + a + " - " + b + " = " + calculate.Subtract(a, b));
+
+            //Multiplication
             Console.WriteLine("Multiplication: " + a + " x " + b + " = " + calculate.Multiply(a, b));
+
+            //Division result will be shown only if result value is not null
             var divisionResult = calculate.Divide(a, b);
             if (divisionResult != null)
             {
                 Console.WriteLine("Division: " + a + " / " + b + " = " + divisionResult);
             }
-
-            
         }
     }
 }
